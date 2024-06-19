496592e67230f768dbc6767230d88d55800b7271
KrantiPatil26 <javafaculty5@fusion-institute.com>
2024-06-19 12:39:12 +0530

    my commit

---
+++ src/arrays/Arr.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Arr {	
+ public static void main(String[] args) {
+		int a[]={90,20,33,40,26};
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
+		//Arrays.sort(a);
+		for(int i=0;i<a.length;i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+}
